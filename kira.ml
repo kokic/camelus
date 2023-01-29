8dfb30b5e194c74af7562f212843255fb679b6d3
kokic <3250679033@qq.com>
2023-01-29 19:33:28 +0800

    parsec - token 2

--- kira.ml
+++ kira.ml
@@ -4,22 +4,23 @@ let parse parser s = match parser s with
 
 let return x input = Some (x, input)
 
-let exactly x s = if x == s.[0] 
-  then return x (String.sub s 1 (String.length s - 1)) 
+let token predicate = fun s -> if predicate s.[0] 
+  then return s.[0] (String.sub s 1 (String.length s - 1)) 
   else None
+
+let exactly x = token ((==) x)
 
 let lbrace = exactly '{'
 
-let tuple_map t f g = (f (fst t), g (snd t))
-let tuple_map' t f = tuple_map t f Fun.id
-let print_tuple t = print_endline ("(\"" ^ fst t ^ "\", \"" ^ snd t ^ "\")")
+let tuple_map f g x = (f (fst x), g (snd x))
+let tuple_map' f x = tuple_map f Fun.id x
+let print_tuple x = print_endline ("(\"" ^ fst x ^ "\", \"" ^ snd x ^ "\")")
 
 let empty = ("_", "_")
-
+let case o f = match o with Some x -> f x | None -> empty
 
 ;; 
-let tuple = match lbrace "{x}" with 
-  Some x -> tuple_map' x Char.escaped | None -> empty in
+let tuple = case (lbrace "{x}") (tuple_map' Char.escaped) in
   print_tuple tuple
 
 
