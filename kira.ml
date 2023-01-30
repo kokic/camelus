e70fc8a19ae3e7f77f083912feb1204d0baba6e0
kokic <3250679033@qq.com>
2023-01-30 16:21:50 +0800

    follow & or

--- kira.ml
+++ kira.ml
@@ -1,36 +1,68 @@
 
-let parse parser s = match parser s with 
-  Some (res, _) -> Some res | None -> None
 
 let return x input = Some (x, input)
 
+let (=?) s = String.length s
+let (|%|) s n = if n >= 0 then n else (=?) s + n
+let subs s off pos = String.sub s off ((s |%| pos) - off + 1)
+let subs' s pos = subs s 0 pos 
+let drops n s = if n >= (=?) s then "" else subs s n ((=?) s - 1)
+let (>>) s n = drops n s
+
 let token predicate = fun s -> if predicate s.[0] 
-  then return s.[0] (String.sub s 1 (String.length s - 1)) 
-  else None
+  then return (Char.escaped s.[0]) (s >> 1) else None
+
+let tokens n predicate = fun s -> let x = subs' s (n - 1) in if predicate x 
+  then return x (s >> n) else None
+
+let inclusive n xs = tokens n (fun x -> (List.exists ((=) x) xs))
+
 let exactly x = token ((==) x)
+let exactly' x = let n = (=?) x in fun s -> if x = subs' s (n - 1)
+  then return x (s >> n) else None
 
-let exactly' x = let n = String.length x in 
-  fun s -> if x == String.sub s 0 n 
-  then return x (String.sub s n (String.length
-   s - 1))
-  else None
+let includes xs = token (fun x -> (List.exists ((==) x) xs))
 
 
+let empty = "_"
+let empty_pair = (empty, empty)
+let empty_pair' = (empty_pair, empty)
 
-let var = exactly' "var"
+let case o otherwise = match o with Some x -> x | _ -> otherwise
+let case_pair o = case o empty_pair
 
-let lbrace = exactly '{'
+let (&) f g x = f (g x)
+
+let destruct x f = match x with Some (x1, x2) -> f x1 x2 | _ -> None
+
+let follow a b source = destruct (a source)
+(fun a1 a2 -> destruct (b a2) (fun b1 b2 -> return (a1, b1) b2))
+
+let either a b source = match a source with Some _ as x -> x | _ -> b source
+
+let (<&>) = follow
+let (<|>) = either
+
+
+
 
 let tuple_map f g x = (f (fst x), g (snd x))
 let tuple_map' f x = tuple_map f Fun.id x
-let print_tuple x = print_endline ("(\"" ^ fst x ^ "\", \"" ^ snd x ^ "\")")
 
-let empty = ("_", "_")
-let case o f = match o with Some x -> f x | None -> empty
-let case' o = match o with Some x -> x | None -> empty 
+
+let string_of_pair x y = ("('" ^ x ^ "', '" ^ y ^ "')")
+let string_of_pair' x = string_of_pair (fst x) (snd x)
+let fflat f x = let a = fst x in (f (fst a) (snd a), snd x)
+let sfflat = fflat (^)
+
+let print_pair x = print_endline (string_of_pair' x)
+
+
+
+let a = exactly 'a'
+let b = exactly 'b'
 
 ;; 
-let tuple = case' (var "var x = 1") in
-  print_tuple tuple
-
+let pair = case (either a b "ba xkcd") empty_pair in
+print_pair (pair)
 
